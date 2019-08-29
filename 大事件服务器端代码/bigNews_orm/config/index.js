5e7d932292e533265fbcc98292d49f5843721559
Aesthetlc <35282196+Aesthetlc@users.noreply.github.com>
2019-08-29 10:18:09 +0800

    服务器代码修改数据库的密码

--- 大事件服务器端代码/bigNews_orm/config/index.js
+++ 大事件服务器端代码/bigNews_orm/config/index.js
@@ -1,10 +1,10 @@
-module.exports ={
+﻿module.exports ={
     // 库名
     database:'bignews',
     // 用户名
     username:'root',
     // 密码
-    password:'',
+    password:'root',
     // 主机
     host:'127.0.0.1',
     // 接口基地址
